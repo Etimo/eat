dc404f413f159bc09578dfeb3cb24900bd19d704
Andre Hansson <andre.hansson@etimo.se>
2025-02-13 10:54:06 +0100

    Added trpc endpoint for next to backend

--- apps/web/src/app/team/page.tsx
+++ apps/web/src/app/team/page.tsx
@@ -1,10 +1,19 @@
+import { getAuthSession } from '@/auth';
 import { NavigationTrail } from '@/components/navigation';
+import { createServerTrpc } from '@/trpc/trpc';
 
 export default async function Page() {
+  const session = await getAuthSession();
+  const trpc = createServerTrpc(session?.accessToken!);
+
+  const teams = await trpc.teams.list.query();
+
   return (
     <main className="flex flex-col gap-5 px-4 pt-4 pb-10">
       <NavigationTrail />
-      Team
+      {teams.map((team) => (
+        <div key={team.id}>{team.name}</div>
+      ))}
     </main>
   );
 }
