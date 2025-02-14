988df234e53139dcadb45677f0ac51ca7bc28620
Morgan Cromell <morgan.cromell@etimo.se>
2025-02-14 11:21:54 +0100

    seed competition and code cleanup

--- apps/auth/src/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './getVerifiedIdentityToken';
-export * from './user';
--- apps/eat-backend/src/seeders/DatabaseSeeder.ts
+++ apps/eat-backend/src/seeders/DatabaseSeeder.ts
@@ -3,9 +3,15 @@ import { Seeder } from '@mikro-orm/seeder';
 import { TeamSeeder } from './TeamSeeder';
 import { UserSeeder } from './UserSeeder';
 import type { EntityManager } from '@mikro-orm/core';
+import { CompetitionSeeder } from './CompetitionSeeder';
 
 export class DatabaseSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
-    this.call(em, [ActivityTypeSeeder, TeamSeeder, UserSeeder]);
+    this.call(em, [
+      CompetitionSeeder,
+      ActivityTypeSeeder,
+      TeamSeeder,
+      UserSeeder,
+    ]);
   }
 }
