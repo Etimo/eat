6279aa072d56a2cc883fe9baeffea11f2e5fadc0
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2025-04-24 15:26:49 +0200

    feat: team cards with basic stats

--- apps/eat-backend/src/trpc/routers/teams-router.ts
+++ apps/eat-backend/src/trpc/routers/teams-router.ts
@@ -1,7 +1,7 @@
 import z from 'zod';
 import { teamData } from '../../data';
 import { protectedProcedure, router } from '../init';
-import { create } from 'domain';
+import dayjs from 'dayjs';
 
 export const teamsRouter = router({
   list: protectedProcedure.query(async ({ ctx }) => {
@@ -20,6 +20,61 @@ export const teamsRouter = router({
       users: teamMemberships.map(({ user }) => ({
         id: user.id,
         name: user.name,
+      })),
+    }));
+  }),
+  listActive: protectedProcedure.query(async ({ ctx }) => {
+    const db = ctx.db;
+    const currentUser = ctx.currentUser;
+
+    const teams = await db.teams.findAll({
+      where: {
+        competition: {
+          startDate: {
+            $lte: dayjs().format('YYYY-MM-DD'),
+          },
+          endDate: {
+            $gte: dayjs().format('YYYY-MM-DD'),
+          }
+        }
+      },
+      populate: ['teamMemberships', 'teamMemberships.user', 'competition'],
+      orderBy: {
+        name: 'ASC',
+      }
+    });
+
+    const activites = await db.activities.findAll({
+      where: {
+        user: { teamMemberships: { team: { id: teams.map((team) => team.id) } } },
+        competition: teams[0].competition
+      }
+    })
+    const stats = activites.reduce((acc, activity) => {
+      const team = teams.find((team) => team.id === team.teamMemberships.find(
+        (teamMembership) => teamMembership.user.id === activity.user.id,
+      )?.team.id);
+      if (!team) return acc;
+      if (!acc[team.id]) {
+        acc[team.id] = {
+          activities: new Set(),
+          minutes: 0,
+        };
+      }
+      acc[team.id].activities.add(activity.activityType.id);
+      acc[team.id].minutes += activity.time;
+      return acc
+    }, {} as Record<string, {minutes: number, activities: Set<string>}>);
+
+    return teams.map(({ id, name, teamMemberships }) => ({
+      id,
+      name,
+      minutes: stats[id]?.minutes ?? 0,
+      activities: stats[id]?.activities.size ?? 0,
+      users: teamMemberships.map(({ user }) => ({
+        id: user.id,
+        name: user.name,
+        isCurrentUser: user.id === currentUser.id,
       })),
     }));
   }),
---
+++ apps/eat-web/src/components/team-activity-card.tsx
@@ -0,0 +1,34 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+type Props = {
+  team: {
+    name: string
+    minutes: number;
+    activities: number
+  }
+}
+export const TeamActivityCard = (props: Props) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{props.team.name}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col gap-1">
+            <div className="flex flex-col">
+              <div className="text-lg font-medium">Total aktivitet</div>
+              <div className="text-xl font-semibold text-green-400">
+                {props.team.minutes} min
+              </div>
+              <div className="text-lg font-medium">Antal aktiviteter</div>
+              <div className="text-xl font-semibold text-green-400">
+                {props.team.activities}
+              </div>
+            </div>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
