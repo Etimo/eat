f0069487d3fb07173d2ed7d5a7aab9401060c589
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2025-04-25 10:59:17 +0200

    chore: add hot reload to vite

--- apps/eat-web/vite.config.ts
+++ apps/eat-web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
@@ -9,5 +10,5 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
   },
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), react()],
 });
